6f21da8524f8ef324791ea2dae9925c954cb39a3
Thamjith <jithuthaha8@gmail.com>
2020-12-09 22:20:54 +0530

    bug fix

--- minesweeper.js
+++ minesweeper.js
@@ -38,21 +38,21 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
             if(squares[i].classList.contains('valid')){
                 // left bomb
-                if(i>0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total++ 
+                if(i > 0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total++ 
                 // top-right bomb
-                if(i>9 && !isRightEdge && squares[i+1-width].classList.contains('bomb')) total++ 
+                if(i > 9 && !isRightEdge && squares[i+1-width].classList.contains('bomb')) total++ 
                 // top
-                if (i > 10 && squares[i-width].classList.contains('bomb')) total ++
+                if (i >= 10 && squares[i-width].classList.contains('bomb')) total ++
                 // top-left
-                if (i > 11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb')) total ++
+                if (i >= 11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb')) total ++
                 // right
-                if (i < 98 && !isRightEdge && squares[i+1].classList.contains('bomb')) total ++
+                if (i <= 98 && !isRightEdge && squares[i+1].classList.contains('bomb')) total ++
                 // bottom-left
                 if (i < 90 && !isLeftEdge && squares[i-1+width].classList.contains('bomb')) total ++
                 // bottom-right
-                if (i < 88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total ++
+                if (i <= 88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total ++
                 // bottom
-                if (i < 89 && squares[i+width].classList.contains('bomb')) total ++
+                if (i <= 89 && squares[i+width].classList.contains('bomb')) total ++
                 squares[i].setAttribute('data', total)
             }
         }
@@ -115,17 +115,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
               const newSquare = document.getElementById(newId)
               click(newSquare)
             }
-            if (currentId > 10) {
+            if (currentId >= 10) {
               const newId = squares[parseInt(currentId -width)].id
               const newSquare = document.getElementById(newId)
               click(newSquare)
             }
-            if (currentId > 11 && !isLeftEdge) {
+            if (currentId >= 11 && !isLeftEdge) {
               const newId = squares[parseInt(currentId) -1 -width].id
               const newSquare = document.getElementById(newId)
               click(newSquare)
             }
-            if (currentId < 98 && !isRightEdge) {
+            if (currentId <= 98 && !isRightEdge) {
               const newId = squares[parseInt(currentId) +1].id
               const newSquare = document.getElementById(newId)
               click(newSquare)
@@ -135,12 +135,12 @@ document.addEventListener('DOMContentLoaded', ()=> {
               const newSquare = document.getElementById(newId)
               click(newSquare)
             }
-            if (currentId < 88 && !isRightEdge) {
+            if (currentId <= 88 && !isRightEdge) {
               const newId = squares[parseInt(currentId) +1 +width].id
               const newSquare = document.getElementById(newId)
               click(newSquare)
             }
-            if (currentId < 89) {
+            if (currentId <= 89) {
               const newId = squares[parseInt(currentId) +width].id
               const newSquare = document.getElementById(newId)
               click(newSquare)
